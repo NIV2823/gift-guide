c7b8bc9ccb1dc38f6c2f71800b4ef879cfd306fd
NIV2823 <2154163277@qq.com>
2026-05-28 16:51:46 +0800

    Remove server configuration and set base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    port: 3000,
-    open: true
-  }
+  base: '/gift-guide/' // 你的仓库名
 })
